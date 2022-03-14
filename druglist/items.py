56860c8328fe6c4fc69d8fadcadebea3ac72ef30
zaochayeming <zaochayeming@hotmail.com>
2022-03-14 17:34:49 +0800

    重写实现方式

--- druglist/items.py
+++ druglist/items.py
@@ -8,4 +8,7 @@ import scrapy
 class DruglistItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+
+
+
     pass
--- druglist/run.py
+++ druglist/run.py
@@ -2,6 +2,8 @@ from scrapy import cmdline
 
 # 运行爬虫
 # cmdline.execute(['scrapy', 'crawl', 'ypk'])
+cmdline.execute(['scrapy', 'crawl', 'ypks'])
+
 # 导出成csv文件
-cmdline.execute(['scrapy', 'crawl', 'ypk', '-o', 'result.csv'])
+# cmdline.execute(['scrapy', 'crawl', 'ypk', '-o', 'result.csv'])
 # cmdline.execute(["scrapy crawl ypk -o result.csv -t csv".split()])
